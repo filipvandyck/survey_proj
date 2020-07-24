eed8f8a0865057087e65ac76a0dbebfd129ab01c
Filip Van Dyck <earthstrikesback@gmail.com>
2020-07-24 08:29:08 +0200

    export pr script add

--- survey/mod_export_pr.py
+++ survey/mod_export_pr.py
@@ -1,40 +1,77 @@
 #mod_export_pr.py
 
 from selenium import webdriver
+import os
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
+
 import geckodriver_autoinstaller
+import time
+import glob
+import shutil
+
 geckodriver_autoinstaller.install() 
 
-options = Options()
-options.headless = True
+OUTPUT_FOLDER      = os.getcwd() + '/selenium/download/'
+OUTPUT_BACK_FOLDER = os.getcwd() + '/selenium/download/backup/'
 
 
 
-print("starting webdriver")
 
-USERNAME = "filip.vandyck@fifthnet.eu"
-PASSWORD = "Telenet1"
+def get_export_pr():
 
-driver = webdriver.Firefox(options=options)
-#driver = webdriver.Firefox(options=options,executable_path='/home/filip/Tools/survey_proj/survey/selenium/geckodriver')
+    files = glob.glob(OUTPUT_FOLDER + "*.csv")
 
-print("loading opdrachten")
-#driver.get("https://"+USERNAME+":"+PASSWORD + "@fifthnet.connectsoftware.nl/tfc/views/opdrachten/opdrachten.php")
-driver.get("https://"+USERNAME+":"+PASSWORD + "@fifthnet.connectsoftware.nl/tfc/views/opdrachten/connectors/grid2csv.php")
-
-
-print("loading export")
-#<div class="dhxtoolbar_text">Acties</div>
-#<div class="btn_sel_text">csv export</div>
-#driver.find_element(By.XPATH, "//div[text()='csv export']").click()
+    for f in files:
+        shutil.move(f, OUTPUT_BACK_FOLDER + 'export_back.csv')
 
 
 
-#driver.get("https://fifthnet.connectsoftware.nl/tfc/views/opdrachten/connectors/grid2xlsx.php")
+    # webdriver firefox options
+    options = Options()
+    options.headless = True
 
-src = driver.page_source
 
-print(src)
+    print('setting firefox profile download dir:')
+    print(OUTPUT_FOLDER)
 
-driver.quit()
+    # webdriver firefox profile
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference('browser.download.folderList', 2)
+    fp.set_preference('browser.download.manager.showWhenStarting', False)
+    fp.set_preference('browser.download.dir', OUTPUT_FOLDER)
+    fp.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document, text/plain, application/pdf, application/vnd.ms-excel, text/csv, text/comma-separated-values, application/octet-stream, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+     #----
+    print("starting webdriver")
+
+    USERNAME = "filip.vandyck@fifthnet.eu"
+    PASSWORD = "Telenet1"
+
+    driver = webdriver.Firefox(options=options,firefox_profile=fp)
+    #driver = webdriver.Firefox(options=options,executable_path='/home/filip/Tools/survey_proj/survey/selenium/geckodriver')
+
+    print("loading opdrachten - get export")
+    driver.get("https://"+USERNAME+":"+PASSWORD + "@fifthnet.connectsoftware.nl/tfc/views/opdrachten/opdrachten.php")
+
+    URL_PR_EXPORT = "https://"+USERNAME+":"+PASSWORD + "@fifthnet.connectsoftware.nl/tfc/views/opdrachten/connectors/grid2csv.php"
+
+    #driver2.get(URL_PR_EXPORT)
+
+
+    driver.execute_script('''window.open("''' + URL_PR_EXPORT + '''","_blank");''')
+
+    #wait for export to finish
+    time.sleep(30)
+
+
+    files = glob.glob(OUTPUT_FOLDER + "*.csv")
+    
+    if files[0] is not None:
+        return files[0]
+    else:
+        print("something went wrong...: " + files)
+
+
+f = get_export_pr()
+print("export written: " + f)
+
+
